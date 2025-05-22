8d75e896713561f550d8ccd198339a59d477cd02
闲愚 <qintong.wqt@antgroup.com>
2025-05-22 16:22:57 +0800

    support framework log level env

--- aworld/logs/util.py
+++ aworld/logs/util.py
@@ -1,41 +1,59 @@
 # coding: utf-8
 # Copyright (c) 2025 inclusionAI.
-
 import logging
+import os
 
+from dotenv import load_dotenv
 
-LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+load_dotenv()
+
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logger = logging.getLogger("common")
 
+framework_log_level = os.getenv("FRAMEWORK_LOG_LEVEL")
+if framework_log_level == "DEBUG":
+    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+    logger.setLevel(logging.DEBUG)
+elif framework_log_level == "INFO":
+    logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
+    logger.setLevel(logging.ERROR)
+elif framework_log_level == "WARNING":
+    logging.basicConfig(level=logging.WARNING, format=LOG_FORMAT)
+    logger.setLevel(logging.WARNING)
+elif framework_log_level == "ERROR":
+    logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
+    logger.setLevel(logging.ERROR)
+else:
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+    logger.setLevel(logging.INFO)
 
 
 class Color:
-    black = '\033[30m'
-    red = '\033[31m'
-    green = '\033[32m'
-    orange = '\033[33m'
-    blue = '\033[34m'
-    purple = '\033[35m'
-    cyan = '\033[36m'
-    lightgrey = '\033[37m'
-    darkgrey = '\033[90m'
-    lightred = '\033[91m'
-    lightgreen = '\033[92m'
-    yellow = '\033[93m'
-    lightblue = '\033[94m'
-    pink = '\033[95m'
-    lightcyan = '\033[96m'
-    reset = '\033[0m'
-    bold = '\033[01m'
-    disable = '\033[02m'
-    underline = '\033[04m'
-    reverse = '\033[07m'
-    strikethrough = '\033[09m'
+    black = "\033[30m"
+    red = "\033[31m"
+    green = "\033[32m"
+    orange = "\033[33m"
+    blue = "\033[34m"
+    purple = "\033[35m"
+    cyan = "\033[36m"
+    lightgrey = "\033[37m"
+    darkgrey = "\033[90m"
+    lightred = "\033[91m"
+    lightgreen = "\033[92m"
+    yellow = "\033[93m"
+    lightblue = "\033[94m"
+    pink = "\033[95m"
+    lightcyan = "\033[96m"
+    reset = "\033[0m"
+    bold = "\033[01m"
+    disable = "\033[02m"
+    underline = "\033[04m"
+    reverse = "\033[07m"
+    strikethrough = "\033[09m"
 
 
 def color_value(value, color: Color = Color.blue, hightlight_key=None):
-    """ Colored value or highlight key in console.
+    """Colored value or highlight key in console.
 
     Args:
         value:
@@ -49,7 +67,7 @@ def color_value(value, color: Color = Color.blue, hightlight_key=None):
 
 
 def color_log(value, color: Color = Color.blue, hightlight_key=None):
-    """ Colored value or highlight key in log.
+    """Colored value or highlight key in log.
 
     Args:
         value:
