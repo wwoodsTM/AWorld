9c3da2202d5440c8984d23a2ce802ad0c4d856f0
文龙 <wenlong.yanwl@antgroup.com>
2025-05-27 15:42:17 +0800

    Add aworld web server

--- run-server.sh
+++ run-server.sh
@@ -2,6 +2,8 @@ pip install -r aworld/requirements.txt
 
 python setup.py install
 
+pip install -r examples/gaia/requirements.txt
+
 pip install -r examples/web/requirements.txt
 
 streamlit run examples/web/main.py
