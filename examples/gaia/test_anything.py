03252df2a2f92fac0aa9102e6f2ed299620951ba
闲愚 <qintong.wqt@antgroup.com>
2025-05-27 16:29:26 +0800

    vector store mcp features

--- examples/gaia/test_anything.py
+++ examples/gaia/test_anything.py
@@ -1,8 +1,10 @@
 import asyncio
+import os
 
 from examples.gaia.mcp_servers.mcp_audio import mcp_audio_metadata
 from examples.gaia.mcp_servers.mcp_browser import browser_use
 from examples.gaia.mcp_servers.mcp_pdf import count_text_occurrences
+from examples.gaia.mcp_servers.mcp_vectore_store import index_text_file, search_text
 from examples.gaia.mcp_servers.mcp_wayback import get_archived_page_content, list_available_versions
 
 
@@ -19,7 +21,9 @@ def test_pdf():
     result = asyncio.run(
         count_text_occurrences(
             file_path=(
-                "/Users/arac/Desktop/AWorld/gaia-benchmark/GAIA/2023/test/8f697523-6988-4c4f-8d72-760a45681f68.pdf"
+                os.path.expanduser(
+                    "~/Desktop/AWorld/gaia-benchmark/GAIA/2023/test/8f697523-6988-4c4f-8d72-760a45681f68.pdf"
+                )
             ),
             text_options=[r"[0-9]+", r"'", r'"', "!"],
         )
@@ -30,14 +34,32 @@ def test_pdf():
 def test_audio():
     result = asyncio.run(
         mcp_audio_metadata(
-            "/Users/arac/Desktop/AWorld/gaia-benchmark/GAIA/2023/test/5bbf523f-b902-4d7d-8e8d-212d00018733.mp3"
+            os.path.expanduser(
+                "~/Desktop/AWorld/gaia-benchmark/GAIA/2023/test/5bbf523f-b902-4d7d-8e8d-212d00018733.mp3"
+            )
         )
     )
     print(result)
 
 
+def test_vector_store():
+    chunks = asyncio.run(
+        index_text_file(
+            file_path=(
+                os.path.expanduser(
+                    "~/Desktop/AWorld/gaia-benchmark/GAIA/2023/test/8f697523-6988-4c4f-8d72-760a45681f68.pdf"
+                )
+            )
+        )
+    )
+    print(f"The number of {chunks=}")
+    results = asyncio.run(search_text(query="Philosophical Discourse of Modernity"))
+    print(f"{results=}")
+
+
 if __name__ == "__main__":
-    # test_wayback()
-    # test_browser()
-    # test_pdf()
+    test_wayback()
+    test_browser()
+    test_pdf()
     test_audio()
+    test_vector_store()
