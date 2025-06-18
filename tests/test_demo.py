1d9ae533754d39adf847afb55de95e67ac4f5aa7
励峰 <honglifeng.hlf@antgroup.com>
2025-06-18 10:14:41 +0800

    gen_pic_server

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,4 +1,15 @@
+import unittest
+from unittest.mock import patch, MagicMock
+import os
 
 
-if __name__ == "__main__":
-    print("hello world")
+from aworld.core.tool.base import ToolFactory
+
+
+class TestShellTool(unittest.TestCase):
+
+
+    def test_init(self):
+        """Test initialization"""
+        self.assertEqual(self.shell_tool.type, "function")
+
