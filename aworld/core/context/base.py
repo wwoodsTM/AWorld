c056a1aef099ccc4fdc64105903554f4ea19af85
道问 <aohui.xah@antgroup.com>
2025-07-23 15:21:44 +0800

    revert

--- aworld/core/context/base.py
+++ aworld/core/context/base.py
@@ -9,7 +9,6 @@ from typing import Dict, Any, TYPE_CHECKING
 
 from aworld.config import ConfigDict
 from aworld.core.context.context_state import ContextState
-from aworld.core.context.agent_state import AgentContext
 from aworld.core.context.session import Session
 from aworld.logs.util import logger
 from aworld.utils.common import nest_dict_counter
@@ -31,7 +30,6 @@ class ContextUsage:
         self.used_context_length = used_context_length
 
 
-@dataclass
 class Context:
     """Context is the core context management class in the AWorld architecture, used to store and manage
     the complete state information of an Agent, including configuration data and runtime state.
@@ -107,30 +105,20 @@ class Context:
     def _init(self, *, task_id: str = None, trace_id: str = None, session: Session = None, engine: str = None):
         self._task_id = task_id
         self._task = None
-
-        # self._engine = engine
-
-        self._session: Session = session
-
-        # share_info
-        self.context_info = ContextState()
-
-        # agent isolated context
-        self._agent_context = Dict[str, AgentContext]
-
-        # self.agent_info = ConfigDict()
-        # self.trajectories = OrderedDict()
-
-        self._swarm = None
-        self._event_manager = None
-
-        # self._trace_state = TraceState()
+        self._engine = engine
         self._trace_id = trace_id
+        self._session: Session = session
+        self.context_info = ContextState()
+        self.agent_info = ConfigDict()
+        self.trajectories = OrderedDict()
         self._token_usage = {
             "completion_tokens": 0,
             "prompt_tokens": 0,
             "total_tokens": 0,
         }
+        # TODO workspace
+        self._swarm = None
+        self._event_manager = None
 
     def add_token(self, usage: Dict[str, int]):
         self._token_usage = nest_dict_counter(self._token_usage, usage)
--- aworld/core/context/session.py
+++ aworld/core/context/session.py
@@ -14,4 +14,5 @@ class Session(BaseModel):
     session_id: str = field(default_factory=lambda: str(uuid.uuid4().hex))
     last_update_time: float = time.time()
     trajectories: List = []
-    messages: Optional[list[MemoryMessage]]
+    # Process Log: Session History of actions and reasoning steps
+    history_messages: Optional[list[MemoryMessage]]
