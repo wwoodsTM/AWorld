d2259676d5a3516698307b58b55ea8a74344b49d
道问 <aohui.xah@antgroup.com>
2025-07-24 16:08:05 +0800

    move state

--- aworld/core/context/state/common.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class ContextUsage(BaseModel):
-    total_context_length: int = 128000
-    used_context_length: int = 0
--- aworld/core/context/state/trace_state.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-
-
-
-class TraceState(BaseModel):
-    trace_id: str = Field(default=None, description="")
